de850284c948500486b4dd81510521a826e4e458
Kylon Tyner <tynerk92@gmail.com>
2022-03-09 10:23:48 -0500

    Added user/register and user/login to API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
 import Express from 'express'
 import UserRoutes from './user.routes'
-import bodyParser from 'body-parser'
 
 const app = Express()
 const port = 3000
@@ -8,15 +7,11 @@ const port = 3000
 app.use(Express.json())
 app.use(Express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.send({
-    text: 'Hello Test',
-  })
-})
-
 app.use('/user', UserRoutes)
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at http://localhost:${port}`)
 })
+
+process.on('exit', () => {})
