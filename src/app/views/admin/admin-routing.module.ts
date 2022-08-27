2b8af6f1d3a890b0cf66f4c44bf0d38f055da398
Kylon Tyner <tynerk92@gmail.com>
2022-08-27 12:29:22 -0400

    Added team member login/dashboard. Fixed login POST to use body

--- src/app/views/admin/admin-routing.module.ts
+++ src/app/views/admin/admin-routing.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common'
 import { RouterModule, Routes } from '@angular/router'
 import { AuthGuard } from 'src/app/_guards/auth.guard'
 import { AdminGuard } from './_guards/admin.guard'
-import { TeamsComponent } from './teams/teams.component'
 import { AdminDashboardComponent } from 'src/app/views/admin/dashboard/admin-dashboard.component'
-import { UsersComponent } from './users/users.component'
 
 const routes: Routes = [
   {
---
+++ src/app/views/team-member/team-member.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { TeamMemberRoutingModule } from './team-member-routing.module'
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, TeamMemberRoutingModule],
+})
+export class TeamMemberModule {}
